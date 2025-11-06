3a1d4f8ce0bb5267f853beecfa00142f0ceb418b
Jose <jmonterrey1993@gmail.com>
2025-11-05 20:41:56 -0400

    deginicion de gets y sets

--- Curso de Js/01 Fundamentos/classes/assets/js/class.js
+++ Curso de Js/01 Fundamentos/classes/assets/js/class.js
@@ -1,11 +1,13 @@
 
 
 class Persona {
- 
+ //segun las reglas es 1 propiedades y metodos estaticos
     nombre = '';
     codigo = '';
     frase  = '';
+    comida = '';
 
+    //segundo la clase constructor
     constructor(nombre= 'no existe', codigo= 'no existe', frase= 'no existe'){
         //console.log('Hola!');
 
@@ -13,7 +15,16 @@ class Persona {
         this.codigo = codigo;
         this.frase  = frase;
     }
+    //sets y gets
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
 
+    get getComidaFavorita(){
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
+    }
+
+    // y los metodos
     quienSoy (){
         console.log(`Soy ${this.nombre} y  mi identidad es ${this.codigo}`);
     }
@@ -34,3 +45,9 @@ ironman.quienSoy();
 
 spiderman.miFrase();
 ironman.miFrase();
+
+spiderman.setComidaFavorita = 'El pie de cereza de tia may';
+
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman)
+

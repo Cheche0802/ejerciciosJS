5b90d91a449f8b556ea82594152a27c56a1f43a7
Jose <jmonterrey1993@gmail.com>
2025-11-25 21:22:01 -0400

    cartas

---
+++ Curso de Js/05-cartas/blackjack-vite/src/blackjack/usescases/crear-deck.js
@@ -0,0 +1,26 @@
+import _ from 'underscore'
+
+export miNombre = 'Fernando';
+
+// Esta función crea un nuevo deck
+const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+
+    let deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
+
+export default crearDeck;
